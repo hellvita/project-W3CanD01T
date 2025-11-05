09b7823f28c99ef7c5b6ecf7a57908f48e30dcbb
hellvita <seredaolga03@gmail.com>
2025-11-05 13:52:08 +0200

    add example of api usage

--- src/js/furniture-api.js
+++ src/js/furniture-api.js
@@ -2,3 +2,13 @@ import axios from 'axios';
 import { BASE_URL, ENDPOINTS } from './constants';
 
 axios.defaults.baseURL = BASE_URL;
+
+export async function fetchFurniture() {
+  const { data } = await axios(`${ENDPOINTS.FURNITURE}`, {
+    params: {
+      page: 1,
+      limit: 8,
+    },
+  });
+  return data;
+}
