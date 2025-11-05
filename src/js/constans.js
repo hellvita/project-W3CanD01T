987bed448285dbd8dab8543e1211523006b1b505
hellvita <seredaolga03@gmail.com>
2025-11-05 13:24:23 +0200

    add base url and endpoints

---
+++ src/js/constans.js
@@ -0,0 +1,8 @@
+export const BASE_URL = 'https://furniture-store-v2.b.goit.study/api';
+
+export const ENDPOINTS = {
+  CATEGORY: '/categories',
+  FURNITURE: '/furnitures',
+  FEEDBACK: '/feedbacks',
+  ORDER: '/orders',
+};
