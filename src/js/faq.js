8290d3e63b257667a06e610b1233cb5da616ed43
Serhii Zviahin <kallisto77@gmail.com>
2025-11-07 21:12:49 +0400

    connect library, design the styles, refactoring faq.html

---
+++ src/js/faq.js
@@ -0,0 +1,11 @@
+import Accordion from "accordion-js";
+// import "accordion-js/dist/accordion.min.css";
+
+new Accordion(".accordion-container");
+// new Accordion(".accordion-container", {
+//   duration: 1000,
+//   showMultiple: true,
+//   onOpen: function (currentElement) {
+//     console.log(currentElement);
+//   }
+// });
