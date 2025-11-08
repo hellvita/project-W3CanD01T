8033361de6d3ad2a8ae6321d48f10032e485c9d4
hellvita <seredaolga03@gmail.com>
2025-11-08 21:55:27 +0200

    add side menu for the header

--- src/js/header-nav.js
+++ src/js/header-nav.js
@@ -2,5 +2,13 @@ import { refs } from './refs';
 import * as handler from './handlers';
 
 export function initHeaderNav() {
+  // add no scroll
   refs.headerMenuBtn.addEventListener('click', handler.onBurgerClick);
+  window.addEventListener('resize', updateModalPosition);
+}
+
+function updateModalPosition() {
+  const headerHeight = refs.header.getBoundingClientRect().height - 0.5;
+
+  refs.headerModal.style.top = `${headerHeight}px`;
 }
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,6 @@
 export const refs = {
+  header: document.querySelector('.header'),
+  headerModal: document.querySelector('.js-header__modal'),
   headerMenuBtn: document.querySelector('.js-header__menu-btn'),
   headerCloseBtn: document.querySelector('.js-header__close-btn'),
 };
