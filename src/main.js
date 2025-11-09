5d9659b4f0aef7bbec4e13db81b5367ca780430c
Serhii Zviahin <kallisto77@gmail.com>
2025-11-09 21:14:49 +0400

    fix paddings of section, create and import render-function markup, const faqData

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+import { faqData } from "./js/constants.js";
 import { initAccordion } from "./js/faq.js";
+import { createFAQ } from "./js/render-functions.js";
 
+createFAQ(faqData);
 initAccordion();
