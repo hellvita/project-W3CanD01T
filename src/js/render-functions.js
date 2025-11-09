4d76e9acdde7064654a40d0e8fec7dd162dd8277
hellvita <seredaolga03@gmail.com>
2025-11-09 23:09:38 +0200

    import sprite

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,4 +1,5 @@
 import { refs } from './refs';
+import icons from '../img/icons.svg';
 
 export function renderFAQ(faqData) {
   const markupFaq = faqData
@@ -7,7 +8,7 @@ export function renderFAQ(faqData) {
             <div class="ac">
                 <h3 class="ac-header">
                     <button type="button" class="ac-trigger">${q}<svg class="svg-sprite-arrow-down" width="17" height="10">
-                                <use href="img/icons.svg#arrow-down"></use>
+                                <use href="${icons}#arrow-down"></use>
                             </svg>
                         </button>
                     </h3>
