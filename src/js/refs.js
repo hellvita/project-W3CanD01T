880be7a7c2282a49ebc4d492e28f4b805019f9c6
oleh skrypnyk <oleh.skrypnyk@ukr.net>
2025-11-10 18:16:21 +0200

    fixed font family

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,10 +1,10 @@
 export const refs = {
-  orderModal: {
+    orderModal: {
     backdrop: document.querySelector('[data-modal-order]'),
     openBtn: document.querySelector('[data-modal-order-open]'),
     closeBtn: document.querySelector('[data-modal-order-close]'),
     form: document.querySelector('[data-order-form]'),
-  },
+    },
     faqSection: {
     accordion: document.querySelector('.accordion-container')
     },
