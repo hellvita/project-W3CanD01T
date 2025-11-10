1c7ed7ab2cc89cc600e66bef525fcbf7cad510d2
Taia Loik <taia.loik9@gmail.com>
2025-11-10 14:53:50 +0200

    Implement new improvements

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import { refs } from './js/refs.js';
 import { initModal } from './js/modal.js';
 import { initOrderForm } from './js/order-form.js';
+import { initSectionFaq } from "./js/faq";
 
 initModal(refs.orderModal);
 initOrderForm();
-import { initSectionFaq } from "./js/faq";
-
 initSectionFaq();
